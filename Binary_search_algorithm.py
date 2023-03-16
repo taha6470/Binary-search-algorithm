75a95b0509a34c44f57fb085831b7773df9c384a
Taha Raza <61325107+taha6470@users.noreply.github.com>
2023-03-16 23:29:00 +0500

    Add files via upload

---
+++ Binary_search_algorithm.py
@@ -0,0 +1,20 @@
+def binary_search(l,target,low=None,high=None):
+    if low is None:
+        low=0
+    if high is None:
+        high=len(l)-1
+    if high<low:
+        return -1
+    midpoint=(low+high)//2
+    if l[midpoint]==target:
+        return midpoint
+    elif target<l[midpoint]:
+        return binary_search(l, target ,low ,midpoint-1)
+    else:
+        return binary_search(l , target,midpoint+1,high)
+if __name__=="__main__":
+    l=[1,2,3,4,5,6,7,8,9,10]
+    target=8
+    print(binary_search(l,target))
+    
+
